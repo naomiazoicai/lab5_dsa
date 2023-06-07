be91e077e9310f57832a204a511a43e2971298d6
naomiazoicai <naomi.azoicai@stud.ubbcluj.ro>
2023-06-07 23:34:32 +0300

    update remove, removeNode

--- SortedIndexedList.cpp
+++ SortedIndexedList.cpp
@@ -12,8 +12,10 @@ SortedIndexedList::SortedIndexedList(Relation r) {
     this->firstEmpty = 0;
     this->capacity = 8;
     this->tree = new Node[8];
+
+    // use the left array to create a singly linked list of empty positions
     for (int i = 0; i < capacity - 1; i++)
-        this->tree[i].leftPos = i + 1;  /// use the left array to create a singly linked list of empty positions
+        this->tree[i].leftPos = i + 1;
     this->tree[capacity - 1].leftPos = -1;
 }
 
@@ -40,10 +42,12 @@ TComp SortedIndexedList::getElement(int i) const{
     //performing a search through the tree array using the leftSubtreeSize values to navigate to the desired position
     while (this->tree[current].leftSubtreeSize != i)
     {
+        //the position we are searching is smaller than the position of the root, so we go in the left subtree
         if (i < this->tree[current].leftSubtreeSize)
             current = this->tree[current].leftPos;
         else
         {
+            //the position we are searching is greater than the position of the root, so we go in the right subtree
             i -= (this->tree[current].leftSubtreeSize + 1);
             current = this->tree[current].rightPos;
         }
@@ -58,38 +62,55 @@ TComp SortedIndexedList::getElement(int i) const{
 TComp SortedIndexedList::remove(int i) {
     if (i < 0 || i >= this->sizeBST)
         throw exception();
-    int current = this->root, parent = -1;
-    while (this->tree[current].leftSubtreeSize != i)  /// find the node to delete
+    int current = this->root;
+    int parent = -1; //keep track of the parent of the current node during traversal
+
+    //find the node to delete
+    while (this->tree[current].leftSubtreeSize != i)
     {
         parent = current;
+        //if the  position is less than leftSubtreeSize, the position is in the left subtree
         if (i < this->tree[current].leftSubtreeSize)
         {
-            this->tree[current].leftSubtreeSize--;  /// decrement the number of nodes from the left subtree of this node
+            //decrement the number of nodes from the left subtree
+            this->tree[current].leftSubtreeSize--;
             current = this->tree[current].leftPos;
         }
         else
+        //if the  position is greater than leftSubtreeSize, the position is in the right subtree
+
         {
+            //adjust the position relative to the right subtree
             i -= (this->tree[current].leftSubtreeSize + 1);
             current = this->tree[current].rightPos;
         }
     }
-    TComp removedElement = this->tree[current].element;  /// save the element which will be removed
+    //the element which will be removed
+    TComp removedElement = this->tree[current].element;
 
-    if (this->tree[current].leftPos == -1 && this->tree[current].rightPos == -1)  /// the node has no descendant
+    // Fall 1: the node has no descendant
+    if (this->tree[current].leftPos == -1 && this->tree[current].rightPos == -1)
     {
         this->removeNode(current, parent, -1);
     }
-    else if (this->tree[current].rightPos == -1)   /// the node has only one descendant on left
+
+    // Fall 2: the node has only one descendant on left
+    else if (this->tree[current].rightPos == -1)
     {
         this->removeNode(current, parent, this->tree[current].leftPos);
     }
-    else if (this->tree[current].leftPos == -1)   /// the node has only one descendant on right
+
+    // Fall 3: the node has only one descendant on right
+    else if (this->tree[current].leftPos == -1)
     {
         this->removeNode(current, parent, this->tree[current].rightPos);
     }
-    else   /// the node has 2 descendants
+    else
+    // Fall 4: the node has 2 descendants
     {
-        /// find the minimum of the right subtree, move it to the node to be deleted, and delete the minimum
+        // find the minimum of the right subtree, move it to the node to be deleted, and delete the minimum
+
+        // find the minimum of the right subtree by traversing the left children of the right subtree until we reach the leftmost node
         int minPos = this->tree[current].rightPos, minParent = current;
         this->tree[minPos].leftSubtreeSize--;
         while (this->tree[minPos].leftPos != -1)
@@ -98,8 +119,8 @@ TComp SortedIndexedList::remove(int i) {
             minPos = this->tree[minPos].leftPos;
             this->tree[minPos].leftSubtreeSize--;
         }
-        this->tree[current].element = this->tree[minPos].element; /// move the minimum
-        this->removeNode(minPos, minParent, this->tree[minPos].rightPos); /// delete the minimum
+        this->tree[current].element = this->tree[minPos].element; // we copy the element of the minimum node to the node to be removed
+        this->removeNode(minPos, minParent, this->tree[minPos].rightPos); // delete the minimum
     }
     this->sizeBST--;
     return removedElement;
@@ -108,12 +129,21 @@ TComp SortedIndexedList::remove(int i) {
 /// Theta(1)
 void SortedIndexedList::removeNode(int nodePos, int parentPos, int nodeChildPos)
 {
+    //nodePos = index of the node to be removed
+    //parentPos = position of the parent node of the node to be removed
+    //nodeChildPos = position of the child node that will replace the node to be removed
     if (nodePos == this->root)
-        this->root = nodeChildPos;
+        this->root = nodeChildPos;  //the child node becomes the new root
+
+        //if the node to be removed is not the root, we check if it is the left child of its parent
     else if (nodePos == this->tree[parentPos].leftPos)
+        //if it is, the left child pointer of the parent is updated to nodeChildPos
         this->tree[parentPos].leftPos = nodeChildPos;
     else
+        //if it isn't, the right child pointer of the parent is updated to nodeChildPos
         this->tree[parentPos].rightPos = nodeChildPos;
+
+    //the removed node is now the first node in the linked list of empty positions
     this->tree[nodePos].leftPos = this->firstEmpty;
     this->firstEmpty = nodePos;
 }
@@ -147,7 +177,9 @@ void SortedIndexedList::add(TComp e) {
         this->resizeTreeArray();
 
     int current = this->root, parent = -1;
-    while (current != -1)   /// find where to insert the node
+
+    // find where to insert the node
+    while (current != -1)
     {
         parent = current;
         if (this->rel(e, tree[current].element))
@@ -160,19 +192,25 @@ void SortedIndexedList::add(TComp e) {
     }
 
     int newPosition = this->firstEmpty;
-    if (current == this->root) /// there was no node in the tree, so add the root
+
+    // there was no node in the tree, so add the root
+    if (current == this->root)
         this->root = newPosition;
     else if (rel(e, tree[parent].element))
         this->tree[parent].leftPos = newPosition;
     else
         this->tree[parent].rightPos = newPosition;
 
-    this->firstEmpty = this->tree[firstEmpty].leftPos;  /// get new firstEmpty position
+    // get new firstEmpty position
+    this->firstEmpty = this->tree[firstEmpty].leftPos;
 
     this->tree[newPosition].element = e;
     this->tree[newPosition].leftSubtreeSize = 0;
-    this->tree[newPosition].leftPos = -1;  /// new node does not have children
+
+    // new node does not have children
+    this->tree[newPosition].leftPos = -1;
     this->tree[newPosition].rightPos = -1;
+
     this->sizeBST++;
 }
 
